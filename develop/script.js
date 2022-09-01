c936e20c5618e902325a20e7c19007069d2e8a5c
William James <Williamj3795@gmail.com>
2022-08-31 22:06:08 -0400

    updated script.js

--- develop/script.js
+++ develop/script.js
@@ -11,14 +11,15 @@ var userChoices;
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+    upperCase = lowerCase.map(toUpper);
 
-// Var To Upper Case 
-var newUpper = [];
+// Var To upperCase/ toUpperCase converts a string to uppercase letters
+var newUpper = []; 
 var toUpper = function (x) {
   return x.toUpperCase();
 };
 
-upperCase = lowerCase.map(toUpper);
+
 //--------------------------------------
 //var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 //var special = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
@@ -55,18 +56,18 @@ function generatePassword() {
     choiceNumber = confirm("Will this contain numbers?");
     console.log("Number " + choiceNumber);
     choiceSpecial = confirm("Will this contain special characters?");
-  console.log("Special Character " + choiceSpecial);
+    console.log("Special Character " + choiceSpecial);
 
   }
-  // if no choice is made
-  if (!choiceLower && !choiceUpper && !choiceNumber && !choiceSpecial) {
-    userChoices = alert("You must choose a criteria");
-  // 4 true options
+  // alerts user if no option is chosen
+  if (!choiceLower && !choiceUpper && !choiceNumber && !choiceSpecial) {  
+    userChoices = alert("You must choose a criteria"); 
+  // 4 user options
   } else if (choiceLower && choiceUpper && choiceNumber && choiceSpecial) {
-    userChoices = lowerCase.concat(upperCase, numbers, special);
+    userChoices = lowerCase.concat(upperCase, numbers, special); // concat() joins mutiple arrays and returns a new array 
     console.log(userChoices);
   }
-  // 3 true options
+  // 3 user options
   else if (choiceLower && choiceUpper && choiceNumber) {
     userChoices = lowerCase.concat(upperCase, numbers);
     console.log(userChoices);
@@ -83,7 +84,7 @@ function generatePassword() {
     userChoices = upperCase.concat(numbers, special);
     console.log(userChoices);
   }
-  // 2 true options
+  // 2 user options
   else if (choiceLower && choiceUpper) {
     userChoices = lowerCase.concat(upperCase);
     console.log(userChoices);
@@ -108,13 +109,13 @@ function generatePassword() {
     userChoices = numbers.concat(special);
     console.log(userChoices);
   }
-   // 1 true option
+   // 1 user option
    else if (choiceLower) {
     userChoices = lowerCase;
     console.log(userChoices);
   }
   else if (choiceUpper) {
-    userChoices = blankUpper.concat(upperCase);
+    userChoices = newUpper.concat(upperCase);
     console.log(userChoices);
   }
   else if (choiceNumber) {
@@ -124,6 +125,5 @@ function generatePassword() {
   else if (choiceSpecial) {
     userChoices = special;
     console.log(userChoices);
-  };
-  
+  }
   ;
